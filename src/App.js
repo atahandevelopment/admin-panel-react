4d96a9064a2d1c69b8039b0d2caf4341843fa7e7
Hasan Atahan <hasan3079.hc@gmail.com>
2024-02-01 13:48:09 +0300

    ----

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { ColorModeContext, useMode } from "./theme";
-import {CssBaseline, ThemeProvider} from "@mui/material";
-import {Routes, Route} from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
@@ -18,35 +18,31 @@ import Calendar from "./scenes/calendar/index";
 function App() {
   const [theme, colorMode] = useMode();
 
-
-  
-
-  return ( 
-  <ColorModeContext.Provider value = {colorMode}>
-                <ThemeProvider theme={theme}>
-                  <CssBaseline/>
-                <div className="app">
-                  <Sidebar/>
-                  <main className="content">
-                    <Topbar/>  
-                    <Routes>
-                      <Route path="/" element={<Dashboard/>}/>
-                      <Route path="/team" element={<Team/>}/>
-                      <Route path="/contacts" element={<Contacts/>}/>  
-                      <Route path="/invoices" element={<Invoices/>}/>
-                      <Route path="/form" element={<Form/>}/>
-                      <Route path="/bar" element={<Bar/>}/>
-                      <Route path="/pie" element={<Pie/>}/>
-                      <Route path="/line" element={<Line/>}/>  
-                      <Route path="/faq" element={<Faq/>}/>
-                      <Route path="/geography" element={<Geography/>}/>
-                      <Route path="/calendar" element={<Calendar/>}/>
-  </Routes>             
-                  </main>
-                </div>
-                </ThemeProvider>
-                
-            </ColorModeContext.Provider>
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<Faq />} />
+              <Route path="/geography" element={<Geography />} />
+              <Route path="/calendar" element={<Calendar />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
